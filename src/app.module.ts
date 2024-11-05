3740c6070a2c89b73e0e58cc58d03a200995ddc4
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-05 15:22:11 -0500

    feat: :sparkles: Add configuration, database, and HTTP setup of AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 // NestJS
 import { Module } from '@nestjs/common'
 
+// Modules
+import { ConfigModule } from '@nestjs/config'
+import { DatabaseModule } from './database/database.module'
+import { HttpModule } from '@nestjs/axios'
+
 // Controllers
 import { AppController } from './app.controller'
 
+// Config
+import { enviroments } from './config/enviroments'
+import config from './config/config'
+import configSchema from './config/configSchema'
+
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+			load: [config],
+			isGlobal: true,
+			validationSchema: configSchema,
+		}),
+		HttpModule,
+		DatabaseModule,
+	],
 	controllers: [AppController],
 	providers: [],
 })
