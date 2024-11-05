55bff271a65ae63dca94af87205fa44eb023593f
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-05 14:55:06 -0500

    feat: :sparkles: add config files

---
+++ src/config/config.ts
@@ -0,0 +1,14 @@
+import { registerAs } from '@nestjs/config'
+
+export default registerAs('config', () => {
+	return {
+		postgres: {
+			database: process.env.POSTGRES_DB,
+			user: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			port: Number(process.env.POSTGRES_PORT),
+			host: process.env.POSTGRES_HOST,
+		},
+		port: Number(process.env.NEST_PORT),
+	}
+})
