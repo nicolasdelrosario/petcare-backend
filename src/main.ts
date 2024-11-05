751eed31dc5b1ba5ae64f96479597f0b21769c59
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-05 15:15:20 -0500

    feat: :sparkles: Add NestJS application setup and validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+// NestJS
 import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+
+// Modules
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+
+	const configService = app.get(ConfigService)
+	const port = configService.get('nestPort')
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+			transformOptions: {
+				enableImplicitConversion: true,
+			},
+		}),
+	)
+
+	console.log(`Application is running on: http://localhost:${port}`)
+	app.enableCors()
 }
+
 bootstrap()
