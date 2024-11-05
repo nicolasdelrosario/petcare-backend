8a67d9c2ddd1a9aae7de568a44fb3d4f22e909b6
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-05 13:37:54 -0500

    chore: :art: applying lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// NestJS
+import { Module } from '@nestjs/common'
+
+// Controllers
+import { AppController } from './app.controller'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [],
+	controllers: [AppController],
+	providers: [],
 })
 export class AppModule {}
