a5aaf5ec068e74ac4d762e5c1f2f449729320781
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-06 02:13:37 -0500

    feat(auth): :passport_control: add LoginDto with validation for email and password

---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,12 @@
+// Validations
+import { IsString, IsNotEmpty } from 'class-validator'
+
+export class LoginDto {
+	@IsString()
+	@IsNotEmpty()
+	readonly email: string
+
+	@IsString()
+	@IsNotEmpty()
+	readonly password: string
+}
