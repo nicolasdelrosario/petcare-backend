86d486b8a7cabc893d20bfba5aa50f4757891e34
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-08 15:45:36 -0500

    chore(workspaces): :lock: using ParseIntPipe in workspaces

--- src/workspaces/workspaces.controller.ts
+++ src/workspaces/workspaces.controller.ts
@@ -1,5 +1,14 @@
 // NestJS
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common'
+import {
+	Controller,
+	Get,
+	Post,
+	Body,
+	Param,
+	Delete,
+	Put,
+	ParseIntPipe,
+} from '@nestjs/common'
 
 // Services
 import { WorkspacesService } from './workspaces.service'
@@ -7,37 +16,47 @@ import { WorkspacesService } from './workspaces.service'
 // DTOs
 import { CreateWorkspaceDto } from './dto/workspace.dto'
 
+// Entities
+import { Workspace } from './entities/workspace.entity'
+
+// Api Documentation
+import { ApiTags } from '@nestjs/swagger'
+
+@ApiTags('Workspaces')
 @Controller('workspaces')
 export class WorkspacesController {
 	constructor(private readonly workspacesService: WorkspacesService) {}
 
 	// Endpoint para listar todos los workspaces
 	@Get()
-	async findAll() {
+	async findAll(): Promise<Workspace[]> {
 		return this.workspacesService.findAll()
 	}
 
 	// Endpoint para obtener un workspace por su ID
 	@Get(':id')
-	async findById(@Param('id') id: number) {
+	async findById(@Param('id', ParseIntPipe) id: number): Promise<Workspace> {
 		return this.workspacesService.findById(id)
 	}
 
 	// Endpoint para crear un nuevo workspace
 	@Post()
-	async create(@Body() data: CreateWorkspaceDto) {
+	async create(@Body() data: CreateWorkspaceDto): Promise<Workspace> {
 		return this.workspacesService.create(data)
 	}
 
 	// Endpoint para actualizar un workspace
-	@Post(':id')
-	async update(@Param('id') id: number, @Body() changes: CreateWorkspaceDto) {
+	@Put(':id')
+	async update(
+		@Param('id', ParseIntPipe) id: number,
+		@Body() changes: CreateWorkspaceDto,
+	): Promise<Workspace> {
 		return this.workspacesService.update(id, changes)
 	}
 
 	// Endpoint para eliminar un workspace
 	@Delete(':id')
-	async delete(@Param('id') id: number) {
+	async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
 		return this.workspacesService.delete(id)
 	}
 }
