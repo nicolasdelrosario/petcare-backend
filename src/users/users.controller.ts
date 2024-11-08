58a4aadb6ccfc7b0e08d49694be029e564e3fe1e
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-08 15:51:09 -0500

    feat(users): :sparkles: using ParseIntPipe in users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,37 +1,55 @@
 // NestJS
-import { Controller, Get, Put, Patch, Body, Param } from '@nestjs/common'
+import {
+	Controller,
+	Get,
+	Put,
+	Patch,
+	Body,
+	Param,
+	ParseIntPipe,
+} from '@nestjs/common'
 
 // Services
 import { UsersService } from './users.service'
 
+// Entities
+import { User } from './entities/user.entity'
+
 // DTOs
 import { UpdateUserDto } from './dto/user.dto'
 
+// Api Documentation
+import { ApiTags } from '@nestjs/swagger'
+
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
 	// Endpoint para listar todos los usuarios
 	@Get()
-	async findAll() {
+	async findAll(): Promise<User[]> {
 		return this.usersService.findAll()
 	}
 
 	// Endpoint para obtener un usuario por su ID
 	@Get(':id')
-	async findById(@Param('id') id: number) {
+	async findById(@Param('id', ParseIntPipe) id: number): Promise<User> {
 		return this.usersService.findById(id)
 	}
 
 	// Endpoint para actualizar un usuario
 	@Put(':id')
-	async update(@Param('id') id: number, @Body() changes: UpdateUserDto) {
+	async update(
+		@Param('id', ParseIntPipe) id: number,
+		@Body() changes: UpdateUserDto,
+	): Promise<User> {
 		return this.usersService.updateUser(id, changes)
 	}
 
 	// Endpoint para eliminar un usuario (soft delete)
 	@Patch(':id')
-	async delete(@Param('id') id: number) {
+	async delete(@Param('id', ParseIntPipe) id: number): Promise<User> {
 		return this.usersService.softDelete(id)
 	}
 }
