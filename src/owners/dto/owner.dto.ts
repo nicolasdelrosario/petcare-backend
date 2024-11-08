95b0299bc01ea1001fb366983fe30c0f77814ae0
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-08 15:07:53 -0500

    feat(owners): :sparkles: add create abd update owner dto

---
+++ src/owners/dto/owner.dto.ts
@@ -0,0 +1,46 @@
+// Validations
+import {
+	IsEmail,
+	IsNotEmpty,
+	IsNumberString,
+	IsOptional,
+	IsPhoneNumber,
+	IsString,
+	Matches,
+} from 'class-validator'
+
+// API Documentation
+import { ApiProperty, PartialType } from '@nestjs/swagger'
+
+export class CreateOwnerDto {
+	@ApiProperty({ description: 'Nombre del dueño' })
+	@IsNotEmpty()
+	@IsString()
+	name: string
+
+	@ApiProperty({ description: 'DNI' })
+	@IsNumberString()
+	@IsOptional()
+	@IsString()
+	dni: string
+
+	@ApiProperty({ description: 'Correo' })
+	@IsEmail()
+	@IsOptional()
+	@Matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,3}$/g)
+	email: string
+
+	@ApiProperty({ description: 'Dirección' })
+	@IsOptional()
+	@IsString()
+	address: string
+
+	@ApiProperty({ description: 'Teléfono' })
+	@IsNotEmpty()
+	@IsNumberString()
+	@IsPhoneNumber('PE')
+	@Matches(/^(9|8|7|3)[0-9]{8}$/)
+	phone: string
+}
+
+export class UpdateOwnerDto extends PartialType(CreateOwnerDto) {}
