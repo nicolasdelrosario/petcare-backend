31b92d6f3a70bb9e8032219989e063e6b2f8b6dc
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-08 15:16:20 -0500

    feat(appointments): :sparkles: add create and update appointment dto

---
+++ src/appointments/dto/appointment.dto.ts
@@ -0,0 +1,63 @@
+// Validations
+import {
+	IsBoolean,
+	IsDate,
+	IsOptional,
+	IsString,
+	IsNotEmpty,
+} from 'class-validator'
+
+// API Documentation
+import { ApiProperty, PartialType } from '@nestjs/swagger'
+
+export class CreateAppointmentDto {
+	@ApiProperty({
+		description: 'Fecha del Appointment en formato YYYY-MM-DD',
+		example: '2024-10-10',
+	})
+	@IsDate()
+	@IsNotEmpty()
+	date: Date
+
+	@ApiProperty({
+		description: 'Hora del Appointment en formato HH:MM:SS',
+		example: '14:30:00',
+	})
+	@IsString()
+	@IsNotEmpty()
+	time: string
+
+	@ApiProperty({
+		description:
+			'Estado del Appointment (true para activo, false para cancelado)',
+		example: true,
+		default: true,
+	})
+	@IsBoolean()
+	@IsOptional()
+	status: boolean
+
+	@ApiProperty({
+		description: 'Razón de la cita, una descripción opcional',
+		example: 'Revisión general',
+	})
+	@IsString()
+	@IsOptional()
+	reason: string
+
+	@ApiProperty({
+		description: 'ID del pet asociado a la cita',
+		example: 1,
+	})
+	@IsNotEmpty()
+	petId: number
+
+	@ApiProperty({
+		description: 'ID del usuario que crea o gestiona la cita',
+		example: 1,
+	})
+	@IsNotEmpty()
+	userId: number
+}
+
+export class UpdateAppointmentDto extends PartialType(CreateAppointmentDto) {}
