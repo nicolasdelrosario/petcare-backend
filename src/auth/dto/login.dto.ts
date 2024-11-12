d398ce8869c7498322dc4202d1627289d31af51a
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-11-12 10:35:13 -0500

    feat(auth): :sparkles: add Login dto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,12 +1,29 @@
 // Validations
-import { IsString, IsNotEmpty } from 'class-validator'
+import {
+	IsNotEmpty,
+	IsString,
+	IsEmail,
+	Matches,
+	MinLength,
+} from 'class-validator'
+
+// Class-transformer
+import { Transform } from 'class-transformer'
+
+// API Documentation
+import { ApiProperty } from '@nestjs/swagger'
 
 export class LoginDto {
-	@IsString()
+	@ApiProperty({ description: 'Email del usuario' })
 	@IsNotEmpty()
-	readonly email: string
+	@IsEmail()
+	@Matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,3}$/g)
+	email: string
 
-	@IsString()
+	@ApiProperty({ description: 'Contraseña del usuario' })
 	@IsNotEmpty()
-	readonly password: string
+	@IsString()
+	@MinLength(8)
+	@Transform(({ value }) => value.trim())
+	password: string
 }
