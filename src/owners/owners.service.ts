6cfa8bd4a3f24ecea668160ebc0ec71af8df9dda
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-12-03 12:17:11 -0500

    refactor(owners): :recycle: refactor owners services, now it also checks which workspace it is

--- src/owners/owners.service.ts
+++ src/owners/owners.service.ts
@@ -3,7 +3,7 @@ import { Injectable, NotFoundException } from '@nestjs/common'
 
 // TypeORM
 import { InjectRepository } from '@nestjs/typeorm'
-import { Repository, IsNull } from 'typeorm'
+import { Repository } from 'typeorm'
 
 // Class-transformer
 import { instanceToPlain } from 'class-transformer'
@@ -14,44 +14,60 @@ import { Owner } from './entities/owner.entity'
 // DTOs
 import { CreateOwnerDto, UpdateOwnerDto } from './dto/owner.dto'
 
+// Interfaces
+import { UserActiveI } from 'src/common/interfaces/user-active-interface'
+
 @Injectable()
 export class OwnersService {
 	constructor(
-		@InjectRepository(Owner) private ownersRepository: Repository<Owner>,
+		@InjectRepository(Owner) private ownerRepository: Repository<Owner>,
 	) {}
 
-	async findAll(): Promise<Owner[]> {
-		return await this.ownersRepository.find({
-			where: { deletedAt: IsNull() },
+	async findAll(user: UserActiveI) {
+		return await this.ownerRepository.find({
+			where: { workspaceId: user.workspaceId },
 		})
 	}
 
-	async findById(id: number): Promise<any> {
-		const owner = await this.ownersRepository.findOne({
-			where: { id, deletedAt: IsNull() },
+	async findOneById(id: number, user: UserActiveI) {
+		const owner = await this.findOwnerById(id, user.workspaceId)
+		return instanceToPlain(owner)
+	}
+
+	async createOwner(createOwnerDto: CreateOwnerDto, user: UserActiveI) {
+		const owner = this.ownerRepository.create(createOwnerDto)
+
+		owner.workspaceId = user.workspaceId
+
+		return await this.ownerRepository.save(owner)
+	}
+
+	async updateOwner(
+		id: number,
+		updateOwnerDto: UpdateOwnerDto,
+		user: UserActiveI,
+	) {
+		await this.findOwnerById(id, user.workspaceId)
+
+		return await this.ownerRepository.update(id, {
+			...updateOwnerDto,
+			workspaceId: user.workspaceId,
+		})
+	}
+
+	async softDelete(id: number, user: UserActiveI) {
+		await this.findOwnerById(id, user.workspaceId)
+		return await this.ownerRepository.softDelete(id)
+	}
+
+	private async findOwnerById(id: number, workspaceId: number): Promise<Owner> {
+		const owner = await this.ownerRepository.findOne({
+			where: { id, workspaceId },
 			relations: ['pets'],
 		})
 
 		if (!owner) throw new NotFoundException('Owner not found')
 
-		const ownerRes = instanceToPlain(owner)
-
-		return ownerRes
-	}
-
-	async createOwner(createOwnerDto: CreateOwnerDto): Promise<Owner> {
-		const owner = this.ownersRepository.create(createOwnerDto)
-		return await this.ownersRepository.save(owner)
-	}
-
-	async updateOwner(id: number, changes: UpdateOwnerDto): Promise<Owner> {
-		await this.ownersRepository.update({ id }, { ...changes })
-		return this.findById(id)
-	}
-
-	async softDelete(id: number): Promise<Owner> {
-		const owner = await this.findById(id)
-		owner.deletedAt = new Date()
-		return await this.ownersRepository.save(owner)
+		return owner
 	}
 }
