c0971927adb20f39dd7f1df7fb79be1fcd581757
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-12-03 12:16:49 -0500

    refactor(pets): :recycle: refactor pets services, now it also checkcs which workspace it is

--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -6,7 +6,7 @@ import { instanceToPlain } from 'class-transformer'
 
 // TypeORM
 import { InjectRepository } from '@nestjs/typeorm'
-import { Repository, IsNull } from 'typeorm'
+import { Repository } from 'typeorm'
 
 // Entities
 import { Pet } from './entities/pet.entity'
@@ -14,6 +14,9 @@ import { Owner } from 'src/owners/entities/owner.entity'
 
 // DTOs
 import { CreatePetDto, UpdatePetDto } from './dto/pet.dto'
+
+// Interfaces
+import { UserActiveI } from 'src/common/interfaces/user-active-interface'
 
 @Injectable()
 export class PetsService {
@@ -24,52 +27,61 @@ export class PetsService {
 		private readonly ownerRepository: Repository<Owner>,
 	) {}
 
-	async findAll(): Promise<Partial<Pet>[]> {
+	async findAll(user: UserActiveI) {
 		const pets = await this.petRepository.find({
-			where: { deletedAt: IsNull() },
+			where: { workspaceId: user.workspaceId },
 		})
-
 		return pets.map(pet => instanceToPlain(pet))
 	}
 
-	async findById(id: number): Promise<Partial<Pet>> {
+	async findOneById(id: number, user: UserActiveI) {
+		const pet = await this.findPetById(id, user.workspaceId)
+		return instanceToPlain(pet)
+	}
+
+	async createPet(data: CreatePetDto, user: UserActiveI) {
+		const pet = this.petRepository.create(data)
+
+		if (data.ownerId)
+			pet.owner = await this.validateOwner(data.ownerId, user.workspaceId)
+
+		pet.workspaceId = user.workspaceId
+
+		return await this.petRepository.save(pet)
+	}
+
+	async updatePet(id: number, updatePetDto: UpdatePetDto, user: UserActiveI) {
+		await this.findPetById(id, user.workspaceId)
+
+		return await this.petRepository.update(id, {
+			...updatePetDto,
+			workspaceId: user.workspaceId,
+		})
+	}
+
+	async softDelete(id: number, user: UserActiveI) {
+		await this.findPetById(id, user.workspaceId)
+		return await this.petRepository.softDelete(id)
+	}
+
+	private async findPetById(id: number, workspaceId: number) {
 		const pet = await this.petRepository.findOne({
-			where: { id, deletedAt: IsNull() },
+			where: { id, workspaceId },
 			relations: ['owner', 'appointments'],
 		})
 
 		if (!pet) throw new NotFoundException(`Pet with id #${id} not found`)
 
-		const petRes = instanceToPlain(pet)
-
-		return petRes
+		return pet
 	}
 
-	async createPet(data: CreatePetDto): Promise<Pet> {
-		const pet = this.petRepository.create(data)
+	private async validateOwner(id: number, workspaceId: number) {
+		const owner = await this.ownerRepository.findOne({
+			where: { id, workspaceId },
+		})
 
-		if (data.ownerId) {
-			const owner = await this.ownerRepository.findOne({
-				where: { id: data.ownerId },
-			})
+		if (!owner) throw new NotFoundException(`Owner with id #${id} not found`)
 
-			if (!owner)
-				throw new NotFoundException(`Owner with id #${data.ownerId} not found`)
-
-			pet.owner = owner
-		}
-
-		return await this.petRepository.save(pet)
-	}
-
-	async updatePet(id: number, changes: UpdatePetDto): Promise<Partial<Pet>> {
-		await this.petRepository.update({ id }, { ...changes })
-		return this.findById(id)
-	}
-
-	async softDelete(id: number): Promise<Pet> {
-		const pet = await this.findById(id)
-		pet.deletedAt = new Date()
-		return await this.petRepository.save(pet)
+		return owner
 	}
 }
